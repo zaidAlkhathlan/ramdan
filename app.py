65aadfe52f44bcb9fd7276e1aa03dfad3592f79c
zaidAlkhathlan <444001475@sm.imamu.edu.sa>
2025-03-05 21:09:00 +0300

    Day 5

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def can_show_riddle():
 ##############################
 if not firebase_admin._apps:
     # Load secrets from Streamlit
-    cred = credentials.Certificate(json.loads(st.secrets["firebase"]))
+    cred = credentials.Certificate(st.secrets["firebase"])
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
