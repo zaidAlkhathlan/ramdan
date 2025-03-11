6ecf1e3d51a98e2ee16b1be1593ea74b06ee678c
zaidAlkhathlan <444001475@sm.imamu.edu.sa>
2025-03-11 18:00:38 +0300

    Day 11

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ def can_show_riddle():
     now = datetime.datetime.now(local_tz)
 
     # Define the allowed window: 9:00 PM → 9:05 PM
-    start_time = now.replace(hour=19, minute=0, second=0, microsecond=0)
-    end_time   = now.replace(hour=19, minute=5, second=0, microsecond=0)
+    start_time = now.replace(hour=18, minute=5, second=0, microsecond=0)
+    end_time   = now.replace(hour=18, minute=10, second=0, microsecond=0)
 
     return start_time <= now <= end_time
 
@@ -55,9 +55,9 @@ db = firestore.client()
 ##############################
 # 🎯 Every day, update this section with a new riddle before uploading
 RIDDLE = {
-    "question": "كم مره ذكر كلمة الربا في القران",
-    "options": ["9", "7", "8", "6"],
-    "answer": "8"
+    "question": "من هو النبي الذي امن به جميع قومه؟",
+    "options": [" موسى عليه السلام", " عيسى عليه السلام", " يونس عليه السلام", " نوح عليه السلام"],
+    "answer": " يونس عليه السلام"
 }
 
 ##############################
