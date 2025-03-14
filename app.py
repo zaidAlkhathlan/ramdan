40b68b52751ef0860f6eeab5837aef68f93e5cfa
zaidAlkhathlan <444001475@sm.imamu.edu.sa>
2025-03-14 17:38:33 +0300

    Day 14

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ def can_show_riddle():
     now = datetime.datetime.now(local_tz)
 
     # Define the allowed window: 9:00 PM → 9:05 PM
-    start_time = now.replace(hour=19, minute=0, second=0, microsecond=0)
-    end_time   = now.replace(hour=19, minute=5, second=0, microsecond=0)
+    start_time = now.replace(hour=17, minute=40, second=0, microsecond=0)
+    end_time   = now.replace(hour=17, minute=45, second=0, microsecond=0)
 
     return start_time <= now <= end_time
 
@@ -55,9 +55,9 @@ db = firestore.client()
 ##############################
 # 🎯 Every day, update this section with a new riddle before uploading
 RIDDLE = {
-    "question": "ما هي أكبر دولة منتجة للزيتون في العالم؟",
-    "options": ["إسبانيا", "إيطاليا", "اليونان", "تركيا"],
-    "answer": "إسبانيا"
+    "question": "كم مرة تأهلت السعودية لكأس العالم؟",
+    "options": ["7", "6", "8", "9"],
+    "answer": "6"
 }
 ##############################
 #      USER AUTH SECTION     #
