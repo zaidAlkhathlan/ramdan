651e66ae1106758dd5b036c011a4b7b57f5c0aa1
zaidAlkhathlan <444001475@sm.imamu.edu.sa>
2025-03-19 17:30:58 +0300

    Day 19

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ def can_show_riddle():
     now = datetime.datetime.now(local_tz)
 
     # Define the allowed window: 9:00 PM → 9:05 PM
-    start_time = now.replace(hour=17, minute=30, second=0, microsecond=0)
-    end_time   = now.replace(hour=17, minute=35, second=0, microsecond=0)
+    start_time = now.replace(hour=17, minute=40, second=0, microsecond=0)
+    end_time   = now.replace(hour=17, minute=45, second=0, microsecond=0)
 
     return start_time <= now <= end_time
 
