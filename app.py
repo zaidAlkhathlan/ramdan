e78aea903f270a6c038cfffbc786f561520b2b46
zaidAlkhathlan <444001475@sm.imamu.edu.sa>
2025-03-24 17:18:34 +0300

    Day 24

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ def can_show_riddle():
     now = datetime.datetime.now(local_tz)
 
     # Define the allowed window: 9:00 PM → 9:05 PM
-    start_time = now.replace(hour=19, minute=0, second=0, microsecond=0)
-    end_time   = now.replace(hour=19, minute=5, second=0, microsecond=0)
+    start_time = now.replace(hour=17, minute=20, second=0, microsecond=0)
+    end_time   = now.replace(hour=17, minute=25, second=0, microsecond=0)
 
     return start_time <= now <= end_time
 
@@ -56,9 +56,9 @@ db = firestore.client()
 # 🎯 Every day, update this section with a new riddle before uploading
 RIDDLE = {
 
-    "question": "ما المقصود بكلمة الفرصاد في اللغة العربية؟",
-     "options": ["التوت", "الكرز", "العنب", "التفاح"],
-     "answer": "التوت"
+    "question": "من هو النبي الذي كانت من معجزاته إحياء الموتى بإذن الله؟",
+     "options": ["نوح عليه السلام", "عيسى عليه السلام", "إبراهيم عليه السلام", "موسى عليه السلام"],
+     "answer": "عيسى عليه السلام"
 
  
 }
