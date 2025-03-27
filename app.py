a549544f4af65a28ccc5af68191ada02f6d782a4
zaidAlkhathlan <444001475@sm.imamu.edu.sa>
2025-03-27 18:29:16 +0300

    Day 27

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ def can_show_riddle():
     now = datetime.datetime.now(local_tz)
 
     # Define the allowed window: 9:00 PM → 9:05 PM
-    start_time = now.replace(hour=21, minute=0, second=0, microsecond=0)
-    end_time   = now.replace(hour=21, minute=5, second=0, microsecond=0)
+    start_time = now.replace(hour=18, minute=30, second=0, microsecond=0)
+    end_time   = now.replace(hour=18, minute=35, second=0, microsecond=0)
 
     return start_time <= now <= end_time
 
@@ -56,12 +56,13 @@ db = firestore.client()
 # 🎯 Every day, update this section with a new riddle before uploading
 RIDDLE = {
 
-    "question": "ما هي أول عاصمة للسعودية؟",
-     "options": ["الرياض", "مكة المكرمة", "الدرعية", "المدينة المنورة"],
-     "answer": "الدرعية"
+    "question": "كم عدد الجيوب الأنفية لدى الإنسان؟",
+     "options": ["4", "6", "8", "10"],
+     "answer": "8"
 
  
 }
+
 
 
 
